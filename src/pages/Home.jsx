ec6ac5b39dae40f457141efe3513eedaa0c6f9b4
Santiago Salazar <santiagosalazar.dev@gmail.com>
2024-05-10 18:50:09 -0400

    chore: Remove unused ContentLoader component and update loading message in Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { fetchApi } from '../utils/data';
 import Global_Layout from '../components/Global_Layout';
 import UserTable from '../components/UserTable';
 import Cookies from 'js-cookie';
-import ContentLoader from 'react-content-loader';
 import './../assets/modules/table.module.css';
 
 const Home = () => {
@@ -36,29 +35,9 @@ const Home = () => {
 	return (
 		<Global_Layout>
 			<h1>Hello, {user.name}</h1>
-			{true ? <TableSkeleton /> : <UserTable students={students} setStudents={setStudents} edit={edit} setEdit={setEdit} />}
+			{loading ? <h3>Loading...</h3> : <UserTable students={students} setStudents={setStudents} edit={edit} setEdit={setEdit} />}
 		</Global_Layout>
 	);
 };
 
 export default Home;
-
-const TableSkeleton = (props) => (
-	<ContentLoader speed={2} width={720} height={300} viewBox='0 0 720 300' backgroundColor='#f3f3f3' foregroundColor='#ecebeb' {...props}>
-		<circle cx='24' cy='24' r='20' />
-		<rect x='61' y='11' rx='5' ry='5' width='546' height='25' />
-		<circle cx='24' cy='98' r='20' />
-		<rect x='61' y='86' rx='5' ry='5' width='546' height='25' />
-		<circle cx='24' cy='172' r='20' />
-		<rect x='61' y='160' rx='5' ry='5' width='546' height='25' />
-		<circle cx='24' cy='247' r='20' />
-		<rect x='61' y='234' rx='5' ry='5' width='546' height='25' />
-	</ContentLoader>
-);
-
-TableSkeleton.metadata = {
-	name: 'Sridhar Easwaran',
-	github: 'sridhareaswaran',
-	description: 'Dashboard pages',
-	filename: 'TableSkeleton',
-};
